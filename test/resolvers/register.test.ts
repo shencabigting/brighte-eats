36658c412f48f40bc38d3a77fb49173543b21b48
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-11-25 22:59:12 +0800

    Add error test case scenarios for register mutation

--- test/resolvers/register.test.ts
+++ test/resolvers/register.test.ts
@@ -3,30 +3,64 @@ import { resolvers } from '../../src/resolvers';
 
 jest.mock('../../src/models/User');
 
+let register: any;
+let mockInput: {
+    name: string
+    email: string
+    mobile: string
+    postcode: string
+    services: [string]
+};
+
 beforeEach(() => {
     jest.resetAllMocks();
+
+    register = resolvers.Mutation.register;
+    mockInput = {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        mobile: '1234567890',
+        postcode: '0000',
+        services: ['PICKUP']
+    };
 });
 
 describe('register Mutation', () => {
     it('should create a user successfully', async () => {
-        // Mock input and database response
-        const mockInput = {
-            name: 'John Doe',
-            email: 'john.doe@example.com',
-            mobile: '1234567890',
-            postcode: '0000',
-            services: ['PICKUP']
-        };
         const mockUser = { id: 1, ...mockInput };
 
-        // Mock the create function
-        (User.create as jest.Mock).mockResolvedValue(mockUser);
+        // Mock the create function to resolve with the inputed data and an ID
+        (User.create as jest.Mock).mockResolvedValue({ id: 1, ...mockInput });
     
-        // Call resolver
-        const result = await resolvers.Mutation.register(null, { input: mockInput });
+        // Call register mutation
+        const result = await register(null, { input: mockInput });
     
         // Assert database call and result
         expect(User.create).toHaveBeenCalledWith(Object.assign(mockInput, {services: JSON.stringify(mockInput.services)}));
         expect(result).toEqual(mockUser);
-      });
+    });
+
+    it('should throw the corresponding error if duplicate error is encountered', async () => {
+        // Mock the create function to throw a DUPLICATE error exception
+        (User.create as jest.Mock).mockRejectedValue({name: 'SequelizeUniqueConstraintError'});
+
+        // Call register mutation and
+        // assert that register rejects with the expected error
+        await expect(register(null, { input: mockInput })).rejects.toThrow('Email or mobile already registered.');;
+    
+        // Assert database call and result
+        expect(User.create).toHaveBeenCalledWith(Object.assign(mockInput, {services: JSON.stringify(mockInput.services)}));
+    });
+
+    it('should throw generic error if any other error is encountered', async () => {
+        // Mock the create function to throw a generic error
+        (User.create as jest.Mock).mockRejectedValue(new Error('DB Error'));
+
+        // Call register mutation and
+        // assert that register rejects with the expected error
+        await expect(register(null, { input: mockInput })).rejects.toThrow('Failed to register new user preference.');;
+    
+        // Assert database call and result
+        expect(User.create).toHaveBeenCalledWith(Object.assign(mockInput, {services: JSON.stringify(mockInput.services)}));
+    });
 });
