0d8f4a78feb5a306b678d6a0a784a8f39d2d4dc9
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-11-26 17:47:35 +0800

    Add more validations to User model init; Create unit test for User model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -2,47 +2,67 @@ import { DataTypes, Model } from 'sequelize';
 import sequelize from '../sequelize.js';
 
 class User extends Model {
-  public id!: number;
-  public name!: string;
-  public email!: string;
-  public mobile!: string;
-  public postcode!: string;
-  public services!: string;
-  public readonly createdAt!: Date;
-  public readonly updatedAt!: Date;
+    public id!: number;
+    public name!: string;
+    public email!: string;
+    public mobile!: string;
+    public postcode!: string;
+    public services!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
 }
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER.UNSIGNED,
-      autoIncrement: true,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isEmail: true,
+            },
+        },
+        mobile: {
+            type: DataTypes.STRING,
+            unique: true,
+        },
+        postcode: {
+            type: DataTypes.STRING,
+            validate: {
+                isInt: true,
+            },
+        },
+        services: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                isEmptyArray(val:string) {
+                    const parsedData = JSON.parse(val);
+                    if (!Array.isArray(parsedData) || parsedData.length === 0) {
+                        throw new Error("Array of at least 1 value is required.");
+                    }
+                },
+            }
+        },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    mobile: {
-        type: DataTypes.STRING,
-        unique: true,
-    },
-    postcode: {
-        type: DataTypes.STRING,
-    },
-    services: {
-        type: DataTypes.STRING,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'user',
-  }
+    {
+        sequelize,
+        tableName: 'user',
+    }
 );
 
 export default User;
---
+++ server/test/models/User.test.ts
@@ -0,0 +1,116 @@
+import User from '../../src/models/User';
+
+let mockData: {
+    name: string
+    email: string
+    mobile: string
+    postcode: string
+    services: string
+};
+
+beforeEach(() => {
+    mockData = {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        mobile: '1234567890',
+        postcode: '0000',
+        services: JSON.stringify(['PICKUP']),
+    };
+});
+
+describe.only('User model validations', () => {
+    describe('name field validations', () => {
+        it('should throw an error if name is null', async () => {
+            try {
+                delete mockData.name;
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('User.name cannot be null');
+            }
+        });
+
+        it('should throw an error if name is empty string', async () => {
+            try {
+                mockData.name = '';
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('Validation notEmpty on name failed');
+            }
+        });
+    });
+
+    describe('email field validations', () => {
+        it('should throw an error if email is null', async () => {
+            try {
+                delete mockData.email;
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('User.email cannot be null');
+            }
+        });
+
+        it('should throw an error if email is empty string', async () => {
+            try {
+                mockData.email = '';
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('Validation notEmpty on email failed');
+            }
+        });
+    });
+
+    describe('postcode field validations', () => {
+        it('should not throw an error if postcode is null', async () => {
+            try {
+                delete mockData.postcode;
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBe(undefined);
+            }
+        });
+
+        it('should throw an error if postcode is not an integer value', async () => {
+            try {
+                mockData.postcode = 'ABC';
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('Validation isInt on postcode failed');
+            }
+        });
+    });
+
+    describe('services field validations', () => {
+        it('should not throw an error if services is valid', async () => {
+            try {
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBe(undefined);
+            }
+        });
+
+        it('should throw an error if services is null', async () => {
+            try {
+                delete mockData.services;
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('User.services cannot be null');
+            }
+        });
+
+        it('should throw an error if services is not valid array', async () => {
+            try {
+                mockData.services = '1';
+                await User.build(mockData).validate();
+            } catch (error) {
+                expect(error).toBeDefined();
+                expect(error.errors[0].message).toBe('Array of at least 1 value is required.');
+            }
+        });
+    });
+});
