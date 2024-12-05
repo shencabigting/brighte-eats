7ec391af8d879abd6e33567089ff839f2e30ac37
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-12-05 19:06:14 +0800

    Debugging

--- infra/main.tf
+++ infra/main.tf
@@ -120,8 +120,8 @@ resource "aws_ecs_task_definition" "frontend_task" {
     image     = "your-frontend-image"
     essential = true
     portMappings = [{
-      containerPort = 80
-      hostPort      = 80
+      containerPort = 3000
+      hostPort      = 3000
     }]
   }])
 }
