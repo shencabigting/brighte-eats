9cea8e9cde75449a0b0bf6e91848bbcc752e3108
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-12-05 15:18:52 +0800

    Debugging

--- infra/main.tf
+++ infra/main.tf
@@ -8,13 +8,15 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-1a"
 }
 
 resource "aws_subnet" "subnet_2" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-southeast-1b"
 }
 
 resource "aws_db_subnet_group" "default" {
