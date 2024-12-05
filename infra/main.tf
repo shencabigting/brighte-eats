c185ac3e255be09f8615fc4a5ff7255b667fe9c5
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-12-05 20:03:16 +0800

    Debugging

--- infra/main.tf
+++ infra/main.tf
@@ -76,7 +76,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # Attach the AmazonECSTaskExecutionRolePolicy to IAM Role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 data "aws_iam_policy_document" "ecs_assume_role_policy" {
