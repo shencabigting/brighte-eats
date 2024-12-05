72a0e6a7234e424d76c3411d2b2a578c186f7759
Shenlene Cabigting <shenlenecabigting@Shenlenes-MacBook-Air.local>
2024-12-05 13:05:04 +0800

    Update bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket"
+    bucket = "shen-brighte-eats-terraform-state-bucket"
     key    = "terraform.tfstate"
     region = "ap-southeast-1"
     encrypt = true
